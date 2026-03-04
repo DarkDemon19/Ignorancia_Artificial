c135eae2b4dab8057f7a3013e36ac7a44f782ff7
DarkDemon19 <a23310382@live.ceti.mx>
2026-03-03 23:49:29 -0600

    Gateando en pañales

---
+++ 0001 Print() y sus variantes.py
@@ -0,0 +1,7 @@
+# Abraham M.R. 23310382
+# print("¡Hola, Python!") funcion exitosa
+print"Abraham"
+print('Abraham')
+print("Abraham")
+print("Abraham") print(Python)
+print(Abraham)
---
+++ 0002 Print() y sus argumentos.py
@@ -0,0 +1,5 @@
+
+# Abraham M.R. 23310382
+
+print("Programming","Essentials","in", sep="$$$", end="°°°")
+print("Python")
