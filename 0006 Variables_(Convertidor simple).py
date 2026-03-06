d865be6ccbce82ef0d341193f4d6542b8c3d1be0
DarkDemon19 <a23310382@live.ceti.mx>
2026-03-06 16:06:39 -0600

    Caminando y tropezando

---
+++ 0006 Variables_(Convertidor simple).py
@@ -0,0 +1,9 @@
+# Abraham M.R. 23310382
+kilometros = 12.25
+millas = 7.38
+
+millas_a_kilometros = millas * 1.61
+kilometros_a_millas = kilometros / 1.61
+
+print(millas, "millas son equivalentes a", round(millas_a_kilometros, 2), "kilómetros")
+print(kilometros, "kilómetros son equivalentes a", round(kilometros_a_millas, 2), "millas")
---
+++ 0007 Operadores y expresiones.py
@@ -0,0 +1,18 @@
+# Abraham M.R. 23310382
+import os
+os.system('cls')
+##limpia pantalla
+x = 0
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("1.-y =", y)
+
+x = 1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("2.-y =", y)
+
+x = -1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("3.-y =", y)
